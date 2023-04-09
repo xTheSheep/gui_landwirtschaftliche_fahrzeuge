fdb1783d5fcf445e65f6e030696d681e5868182a
Fabian <inf3917@hs-worms.de>
2023-04-09 20:31:14 +0200

    added database, restructured project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-import ressources_interface
+import sqlite3
+from ui_files import ressources_interface
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 from PyQt5.QtGui import QDoubleValidator, QValidator
@@ -9,7 +10,7 @@ from PyQt5.QtGui import QDoubleValidator, QValidator
 class Startscreen(QMainWindow):
     def __init__(self):
         super(Startscreen, self).__init__()
-        loadUi("interface.ui", self)
+        loadUi("./ui_files/interface.ui", self)
         self.stackedWidget.setCurrentIndex(0)
         self.pushButton_4.clicked.connect(self.login)
         self.pushButton.clicked.connect(self.register)
@@ -20,11 +21,28 @@ class Startscreen(QMainWindow):
         self.stackedWidget_mainapp.setCurrentIndex(number)
 
     def login(self):
-        if checklogin(self.input_username.text(), self.input_password.text()):
-            self.stackedWidget.setCurrentIndex(1)  # switch to mainpage
-            self.hint_login_information.setText('')
+        user = self.input_username.text()
+        password = self.input_password.text()
+        if len(user) == 0 or len(password) == 0:
+            self.hint_login_information.setText('Please fill out all fields.')
+
         else:
-            self.hint_login_information.setText('Invalid Data')
+            db = sqlite3.connect("db.db")
+            cursor = db.cursor()
+            query = 'SELECT password, seller FROM users WHERE username =\'' + user + "\'"
+            cursor.execute(query)
+            result = cursor.fetchone()
+            if result[0] == password and result[1] == 0:
+                # set user information
+                self.welcome_label.setText(f'welcome back {user}'.upper())
+                self.stackedWidget.setCurrentIndex(1)  # switch to buyerpage
+                self.hint_login_information.setText('')
+            elif result[0] == password and result[1] == 1:
+                self.welcome_label.setText(f'welcome back {user}'.upper())
+                self.stackedWidget.setCurrentIndex(2)  # switch to sellerpage
+                self.hint_login_information.setText('')
+            else:
+                self.hint_login_information.setText('Invalid Data')
 
     def register(self):
         registerwindow.show()
@@ -33,36 +51,45 @@ class Startscreen(QMainWindow):
 class CreateAccountDialog(QDialog):
     def __init__(self):
         super(CreateAccountDialog, self).__init__()
-        loadUi("newaccountdialog.ui", self)
+        loadUi("./ui_files/newaccountdialog.ui", self)
         self.input_budget.editingFinished.connect(self.validate)
         self.createAccount_button.clicked.connect(self.createAccount)
+
     def validate(self):
         rule = QDoubleValidator(1, 10000000000, 0)
         if rule.validate(self.input_budget.text(), 14)[0] == 2:
             pass
         else:
             self.input_budget.setText('')
+
     def createAccount(self):
-        users.append({
-            'username': self.input_username.text(),
-            'password': self.input_password.text(),
-            'budget': self.input_budget.text(),
-        })
-        self.account_hint.setText('Account has been created!')
+        username = self.input_username.text()
+        password = self.input_password.text()
+        budget = self.input_budget.text()
 
-# login data
-def checklogin(username, password):
-    for user in users:
-        if username == user['username'] and password == user['password']:
-            return True
+        if len(username) == 0 or len(password) == 0 or len(budget) == 0:
+            self.account_hint.setText('Please fill out ALL fields.')
+        elif len(username) < 4:
+            self.account_hint.setText('The username cant be shorter than 4')
+        elif len(password) < 3:
+            self.account_hint.setText('The password is too short')
+        else:
+            db = sqlite3.connect("db.db")
+            cursor = db.cursor()
+            query = 'SELECT username FROM users WHERE username =\'' + username + "\'"
+            cursor.execute(query)
+            if cursor.fetchone() is None:
+                user_info = [username, password, budget, 0]
+                cursor.execute('INSERT INTO users (username, password, budget, seller) VALUES (?, ?, ?, ?)', user_info)
+                db.commit()
+                db.close()
+                self.input_username.setText('')
+                self.input_password.setText('')
+                self.input_budget.setText('')
+                self.account_hint.setText('Account has been created!')
+            else:
+                self.account_hint.setText('Username already exists')
 
-users = [
-    {'username': 'Oskar', 'password': '123', 'budget': 187},
-    {'username': 'Benni', 'password': '123', 'budget': 187},
-    {'username': 'Daniela', 'password': '123', 'budget': 187},
-    {'username': 'Horst', 'password': '123', 'budget': 187},
-    {'username': 'Sieglinde', 'password': '123', 'budget': 187},
-]
 
 # main
 app = QApplication(sys.argv)
